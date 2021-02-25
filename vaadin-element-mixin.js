e4319b857172b8336ae30439f48e375cfaa9d9f4
web-padawan <iamkulykov@gmail.com>
2021-02-25 10:46:25 +0200

    Cleanup imports and globals

--- vaadin-element-mixin.js
+++ vaadin-element-mixin.js
@@ -1,24 +1,19 @@
 import { idlePeriod } from '@polymer/polymer/lib/utils/async.js';
 import { Debouncer, enqueueDebouncer } from '@polymer/polymer/lib/utils/debounce.js';
-import '@polymer/polymer/lib/utils/flush.js';
-
-import { DirMixin } from './vaadin-dir-mixin.js';
 import { usageStatistics } from '@vaadin/vaadin-usage-statistics/vaadin-usage-statistics.js';
-if (!window.Vaadin) {
-  window['Vaadin'] = {};
-}
+import { DirMixin } from './vaadin-dir-mixin.js';
+
+window.Vaadin = window.Vaadin || {};
 
 /**
  * Array of Vaadin custom element classes that have been finalized.
  */
-window['Vaadin'].registrations = window.Vaadin.registrations || [];
+window.Vaadin.registrations = window.Vaadin.registrations || [];
 
-// Use the hack to prevent polymer-modulizer from converting to exports
-window['Vaadin'].developmentModeCallback = window.Vaadin.developmentModeCallback || {};
-window['Vaadin'].developmentModeCallback['vaadin-usage-statistics'] = function() {
-  if (usageStatistics) {
-    usageStatistics();
-  }
+window.Vaadin.developmentModeCallback = window.Vaadin.developmentModeCallback || {};
+
+window.Vaadin.developmentModeCallback['vaadin-usage-statistics'] = function() {
+  usageStatistics();
 };
 
 let statsJob;
